8e2bec2663c9e179e98bbb4c5810b1e66f3a13ee
Brian <brian.m.wendt@gmail.com>
2019-05-06 20:40:00 -0700

    Update schedule.php

--- templates/schedule.php
+++ templates/schedule.php
@@ -1,6 +1,9 @@
-<?php
-$days = $EventsGrid->days();
-foreach ($days as $Day) {
-    include 'header.day.php';
-    include 'table.day.php';
-} 
+<div class="tte-schedule">
+    <?php
+    $days = $EventsGrid->days();
+    foreach ($days as $Day) {
+        include 'header.day.php';
+        include 'table.day.php';
+    }
+    ?>
+</div>
